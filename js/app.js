e15e5a1228c2e4639080df7cb4edf0fabf4b5b12
Nicolas Quinn <nclsquinn@gmail.com>
2022-09-15 13:52:55 -0300

    Added a delete button for each appointment

--- js/app.js
+++ js/app.js
@@ -35,6 +35,10 @@ class Citas {
         this.citas = [...this.citas, cita];
     }
 
+    eliminarCita(id) {
+        this.citas = this.citas.filter( (cita) => cita.id !== id);
+    }
+
 }
 
 class UI {
@@ -102,6 +106,15 @@ class UI {
             <span class="font-weight-bolder">Síntomas:</span> ${sintomas}
             `;
 
+            // Boton para eliminar cita
+            const btnBorrar = document.createElement('BUTTON');
+            btnBorrar.classList.add('btn', 'btn-danger', 'mr-2');
+            btnBorrar.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+            </svg>`;
+
+            btnBorrar.onclick = () => eliminarCita(id);
+
             // Agregar los elementos de scripting al divCita e insertar en el HTML
             divCita.appendChild(mascotaParrafo);
             divCita.appendChild(propietarioParrafo);
@@ -109,6 +122,7 @@ class UI {
             divCita.appendChild(fechaParrafo);
             divCita.appendChild(horaParrafo);
             divCita.appendChild(sintomasParrafo);
+            divCita.appendChild(btnBorrar);
             contenedorCitas.appendChild(divCita);
 
         })
@@ -175,4 +189,10 @@ function reiniciarObjeto () {
     citaObj.fecha = '';
     citaObj.hora = '';
     citaObj.sintomas = '';
+}
+
+function eliminarCita(id) {
+    adminCitas.eliminarCita(id);
+    ui.mostrarAlerta('Cita eliminada exitosamente');
+    ui.mostrarCitas(adminCitas);
 }
